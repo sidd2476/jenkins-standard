aaf49be9c0e45848325c932293cc1f5daa772e37
siddthanth chopra <32588309+sidd2476@users.noreply.github.com>
2023-07-12 13:30:19 +0530

    Update welocme.groovy

--- vars/welocme.groovy
+++ vars/welocme.groovy
@@ -1,3 +1,8 @@
-def call(name){
-  echo "hello ${name} . Welcome!"
-}
+def call() {
+  Map pipelineConfig = readYaml(file: "${WORKSPACE}/Jenkinsfile.yaml }")
+  node {
+    stage('Build'){
+      println "Building: ${pipelineConfig.project_name}"
+    }
+  }
+}  
