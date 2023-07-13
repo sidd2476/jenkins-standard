2270e5744a29198f30b432779acbecdd15cd48c9
siddthanth chopra <32588309+sidd2476@users.noreply.github.com>
2023-07-13 11:44:06 +0530

    Update welocme.groovy

--- vars/welocme.groovy
+++ vars/welocme.groovy
@@ -2,8 +2,13 @@ def call() {
   pipeline {
     stages{
     stage('Verify Workspace'){
+      script{
+      sh """
+      git clone $GIT_URL
       echo "${env.JENKINS_HOME}"
       Map pipelineConfig = readYaml(file: "${WORKSPACE}/Jenkinsfile.yaml")
+      """
+      }
     }
     stage('Build'){
       println "Building: ${pipelineConfig.project_name}"
