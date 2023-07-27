5bceda05e82e7c49f59a049591005e96a4a2df65
siddthanth chopra <32588309+sidd2476@users.noreply.github.com>
2023-07-27 15:48:30 +0530

    Update welocme.groovy

--- vars/welocme.groovy
+++ vars/welocme.groovy
@@ -34,11 +34,11 @@ def call() {
           fi
           
           export INITTAG=$(git describe --abbrev=0 --tags)||true
-          export INITTAGSEMVER=$(semver INITTAG)
-          export INITVERSION=$(semver ${INITTAGSEMVER:-"0.0.0"})
+          export INITTAGSEMVER=$(npx semver INITTAG)
+          export INITVERSION=$(npx semver ${INITTAGSEMVER:-"0.0.0"})
           export fullTag=$(git for-each-ref refs/tags --sort=-creatordate --format='%(refname)'|grep feature |head -1 )||true
           export GIT_TAG_FEATURE=${fullTag##*/}||true
-          export GIT_TAG_FEATURE_SEMVER=$(semver -i ${GIT_TAG_FEATURE:-$INITVERSION})
+          export GIT_TAG_FEATURE_SEMVER=$(npx semver -i ${GIT_TAG_FEATURE:-$INITVERSION})
           echo "GIT_TAG_FEATURE=$GIT_TAG_FEATURE GIT_TAG_FEATURE_SEMVER=$GIT_TAG_FEATURE_SEMVER"
     
           export CURRENT_TAG=$GIT_TAG_FEATURE
@@ -56,9 +56,9 @@ def call() {
           export PRD_TAG_SEMVER=$(semver ${PRD_TAG_SEMVER:-"0.0.0"})
           echo "PRD_TAG_SEMVER=$PRD_TAG_SEMVER"
           echo "CURRENT_TAG=$CURRENT_TAG"
-          export CURRENT_TAG_SEMVER=$(semver -i ${CURRENT_TAG:-$INITVERSION})
+          export CURRENT_TAG_SEMVER=$(npx semver -i ${CURRENT_TAG:-$INITVERSION})
           echo "CURRENT_TAG_SEMVER=$CURRENT_TAG_SEMVER"
-          PRD_VS_CURR=$(pysemver compare $PRD_TAG_SEMVER $CURRENT_TAG_SEMVER  2>&1)
+          PRD_VS_CURR=$(npx pysemver compare $PRD_TAG_SEMVER $CURRENT_TAG_SEMVER  2>&1)
           echo "PRD_VS_CURR=$PRD_VS_CURR"
     
           if (( $PRD_VS_CURR > -1 )); then
@@ -68,7 +68,7 @@ def call() {
             export CURRENT_TAG=$CURRENT_TAG
             echo "Setting CURRENT_TAG=$CURRENT_TAG"
           fi
-          export TAG=${SEMVER_PREFIX:-v}$(semver $CURRENT_TAG -i ${CI_TAG_INCRMENT_KEY} --preid ${CI_TAG_KEYWORD:-feature})
+          export TAG=${SEMVER_PREFIX:-v}$(npx semver $CURRENT_TAG -i ${CI_TAG_INCRMENT_KEY} --preid ${CI_TAG_KEYWORD:-feature})
           git tag -a $TAG -m "version $TAG"
           git push --tags ${GIT_URL}
           '''
